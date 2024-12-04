0896010bccb5f8287567b8223ad064924916b5fe
Max Eagan <meagan7654@gmail.com>
2024-12-03 19:17:59 -0600

    Add utilities with jwt

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     fullName:{type: String},
     email: {type: String},
     password: {type: String},
-    createdOn: {type: Date, default: new Date().getTime() }
+    createdOn: {type: Date, default: new Date().getTime() },
 });
 
 module.exports = mongoose.module("User", userSchema);
